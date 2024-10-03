89fc3f5a7fa24faf9766271f9d2ef5925468cdc1
Samuel Rhea <21045301+TownLake@users.noreply.github.com>
2024-10-03 12:05:48 +0000

    fix config

--- my-next-app/next.config.mjs
+++ my-next-app/next.config.mjs
@@ -1,7 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  target: 'serverless',
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
 }
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
